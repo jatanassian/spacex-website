b27d1fa2bd599fe22c882d79190876e3ae09424a
Julien Atanassian <julien.atanassian@gmail.com>
2022-08-22 12:14:19 -0400

    add stats and logic to increment numbers

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("menu-btn");
 const overlay = document.getElementById("overlay");
 const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
 
 btn.addEventListener("click", navToggle);
 
@@ -10,3 +11,30 @@ function navToggle() {
   document.body.classList.toggle("stop-scrolling");
   menu.classList.toggle("show-menu");
 }
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+      // Get count target
+      const target = Number(counter.getAttribute("data-target"));
+
+      // Get current counter value
+      let c = Number(counter.innerText);
+
+      // If counter less than target, add increment
+      if (c < target) {
+        c++;
+        counter.innerText = `${c}`;
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = `${target}`;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+countUp();
