af5d28795a7009953d4d0f39588454b90b33d6b6
Julien Atanassian <julien.atanassian@gmail.com>
2022-08-22 13:46:10 -0400

    trigger counter on scroll position

--- script.js
+++ script.js
@@ -4,12 +4,27 @@ const menu = document.getElementById("mobile-menu");
 const counters = document.querySelectorAll(".counter");
 
 btn.addEventListener("click", navToggle);
+document.addEventListener("scroll", scrollPage);
+
+let scrollStarted = false;
 
 function navToggle() {
   btn.classList.toggle("open");
   overlay.classList.toggle("overlay-show");
   document.body.classList.toggle("stop-scrolling");
   menu.classList.toggle("show-menu");
+}
+
+function scrollPage() {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPosition < 100) {
+    resetCount();
+    scrollStarted = false;
+  }
 }
 
 function countUp() {
@@ -37,4 +52,6 @@ function countUp() {
   });
 }
 
-countUp();
+function resetCount() {
+  counters.forEach((counter) => (counter.innerText = "0"));
+}
